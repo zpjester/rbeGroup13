714bcf4744abcba86ad94b4cbff7d3473f7852ae
zpjester <58483011+zpjester@users.noreply.github.com>
2019-12-09 22:58:38 -0500

    Clean-up of code

--- FinalRobotCode/src/main.cpp
+++ FinalRobotCode/src/main.cpp
@@ -11,76 +11,7 @@
 // R_Line               line          G               
 // backBump             bumper        A               
 // ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// L_Drive              motor         2               
-// R_Drive              motor         1               
-// Controller1          controller                    
-// Arm_A                motor         17              
-// Arm_B                motor         18              
-// Gripper_M            motor         6               
-// L_Line               line          H               
-// R_Line               line          G               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// L_Drive              motor         2               
-// R_Drive              motor         1               
-// Controller1          controller                    
-// Arm_A                motor         17              
-// Arm_B                motor         18              
-// Gripper_M            motor         6               
-// L_Line               line          F               
-// R_Line               line          G               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// L_Drive              motor         2               
-// R_Drive              motor         1               
-// Controller1          controller                    
-// Arm_A                motor         17              
-// Arm_B                motor         18              
-// Gripper_M            motor         6               
-// L_Line               line          F               
-// R_Line               line          H               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// L_Drive              motor         2               
-// R_Drive              motor         1               
-// Controller1          controller                    
-// Arm_A                motor         17              
-// Arm_B                motor         18              
-// Gripper_M            motor         6               
-// L_Line               line          G               
-// R_Line               line          H               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// L_Drive              motor         2               
-// R_Drive              motor         1               
-// Controller1          controller                    
-// Arm_A                motor         17              
-// Arm_B                motor         18              
-// Gripper_M            motor         6               
-// L_Line               line          G               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       VEX                                                       */
-/*    Created:      Thu Sep 26 2019                                           */
-/*    Description:  Competition Template                                      */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
 
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 #include "declarations.h"
@@ -112,15 +43,6 @@ competition Competition;
 
 // define your global instances of motors and other devices here
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
@@ -136,39 +58,18 @@ void pre_auton(void) {
   lift.speedRatio = .135;
   
   lift.resetArmPos();
-
-  
-  
-  
-
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+
 
 void autonomous(void) {
   Brain.Screen.print(test);
   autoRoutine1.runAuto();
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+
 
 void usercontrol(void) {
   // User control code here, inside the loop
