49845921ff5a9700f4ed792901891ee8276bdf38
zpjester <58483011+zpjester@users.noreply.github.com>
2019-12-14 00:01:56 -0500

    Code commented
    Fully commented

--- FinalRobotCode/src/encoderDrive.cpp
+++ FinalRobotCode/src/encoderDrive.cpp
@@ -7,57 +7,59 @@ private:
 
 
 public:
+//Drive train parameters
 float wheelRadius=2;
 float drivenGear = 10;
 float driveGear = 4;
 float gearRatio=(driveGear/drivenGear);
 
 
-float calculateRotations(double targetDistance){
+float calculateRotations(double targetDistance){//Calculates motor rotations needed to drive a wheel a certain distance
     return targetDistance/((wheelRadius*2*3.1416))/gearRatio;
 }
 
 
 
-void driveArc(float radius, char driveDirection, char turnDirection, float angle, bool await){
+void driveArc(float radius, char driveDirection, char turnDirection, float angle, bool await){//Drives the robot along an arc of set radius, direction and angle.
     
-    
+    //Robot parameters
     float treadWidth=13.75;//inches
     float motorMaxSpeed=75;//percent
     
+    //Find the distance for each motor to drive
     float IWheelDistance=(radius - treadWidth/2)*3.1416*2*angle/360;//inches
     float OWheelDistance=(radius + treadWidth/2)*3.1416*2*angle/360;//inches
     
-    
+    //Finds the number of rotations for each motor to drive
     float IWheelRotations=calculateRotations(IWheelDistance);
     float OWheelRotations=calculateRotations(OWheelDistance);
     
-    if (driveDirection=='b'){
+    if (driveDirection=='b'){//If drive direction is backwards, reverse the motor directions
         IWheelRotations=IWheelRotations*-1;
         OWheelRotations=OWheelRotations*-1;
     }
     
     
     if(turnDirection=='r'){
-        L_Drive.setVelocity(motorMaxSpeed, percent);
+        L_Drive.setVelocity(motorMaxSpeed, percent);//Start driving
         R_Drive.setVelocity(motorMaxSpeed*IWheelDistance/OWheelDistance, percent);
         L_Drive.spinFor(OWheelRotations, turns, false);
         R_Drive.spinFor(IWheelRotations, turns, await);
     }
     else if(turnDirection=='l'){
-        R_Drive.setVelocity(motorMaxSpeed, percent);
+        R_Drive.setVelocity(motorMaxSpeed, percent);//Start driving
         L_Drive.setVelocity(motorMaxSpeed*IWheelDistance/OWheelDistance, percent);
         R_Drive.spinFor(OWheelRotations, turns, false);
         L_Drive.spinFor(IWheelRotations, turns, await);
         
     }
-    else{
+    else{//Send a print if the turn direction is invalid
         Brain.Screen.print("Error, invalid turn direction");
         task::sleep(1000);
     }
 }
 
-void driveDistance(float distance, bool await){
+void driveDistance(float distance, bool await){//Drives a ser distance
     float motorMaxSpeed=100;//percent
     float motorRot = calculateRotations(distance);
     R_Drive.setVelocity(motorMaxSpeed, percent);
--- FinalRobotCode/src/gripper.cpp
+++ FinalRobotCode/src/gripper.cpp
@@ -4,68 +4,68 @@
 
 class rollerIntake{
 private:
+//Set thresholds for automatic operation
 double freeTorqueThresh = 0.3;//N*M
 double backTorqueThresh = .6;//N*M
 
 
 public:
 
-void stopGripper(){
+void stopGripper(){//Stop the gripper motor
   Gripper_M.stop();
   
 }
-void runIntake(){
+void runIntake(){//Sets gripper motor to intake
   Gripper_M.spin(forward, 100, pct);
   
 }
-void runEject(){
+void runEject(){//Sets gripper motor to eject
   Gripper_M.spin(reverse, 100, pct);
   
 }
 
 
-double getGripperTorque(){
+double getGripperTorque(){//Reads the torque of the gripper, and returns it
   double mTorque = Gripper_M.torque(Nm);
   return mTorque;
 }
 
-bool smartGripper(std::string direction){
-  if(direction == "In" || direction == "in"){
-    
-    if(getGripperTorque() < backTorqueThresh){
+bool smartGripper(std::string direction){//Runs the gripper in the specified direction, will shut down the gripper and return true if the pizza reaches its intended position (fully gripped or released)
+  if(direction == "In" || direction == "in"){//If set to intake
+    if(getGripperTorque() < backTorqueThresh){//If the gripper is still intaking,
       Brain.Screen.clearScreen();
       Brain.Screen.setCursor(0,0);
       Brain.Screen.print("Intaking");
-      runIntake();
-      return false;     
+      runIntake();//run the intake
+      return false;//and return false
     }
-    else{
+    else{//If the pizza has reached the back of the gripper,
       Brain.Screen.clearScreen();
       Brain.Screen.setCursor(0,0);
-      stopGripper();
-      return true;
+      stopGripper();//stop the intake
+      return true;//and return true
     }
   }
-  else{
+  else{//If set to eject
     runEject();
-    task::sleep(100);
-    if(getGripperTorque() > freeTorqueThresh){
+    task::sleep(100);//Start the motor for a short time, to allow the motor to spin up and build torque
+    if(getGripperTorque() > freeTorqueThresh){//Check the torque, if the motor is loaded,
       Brain.Screen.clearScreen();
       Brain.Screen.setCursor(0,0);
       Brain.Screen.print("Ejecting");
-      runEject();
-      return false;     
+      runEject();//Continue running the motor
+      return false;//and return false
     }
-    else{
+    else{//If motor is free spinning,
       Brain.Screen.clearScreen();
       Brain.Screen.setCursor(0,0);
       task::sleep(100);
-      stopGripper();
-      return true;
+      stopGripper();//Stop the gripper
+      return true;//and return false
     }
   }
 }
-void fullSmartGripper(std::string direction){
+void fullSmartGripper(std::string direction){//Runs the smart gripper command and waits for it to return true.
   bool complete = false;
   while(!complete){
     complete = smartGripper(direction);
